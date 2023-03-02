6a207f4e0a69e4637f279e75a15a46e37eaf22a3
Tsetsee <tsetsee.yugi@gmail.com>
2023-03-03 00:45:41 +0800

    haha

--- src/Controller/QPayQRController.php
+++ src/Controller/QPayQRController.php
@@ -4,31 +4,25 @@ declare(strict_types=1);
 
 namespace Tsetsee\SyliusQpayPlugin\Controller;
 
+use Payum\Bundle\PayumBundle\Controller\PayumController;
 use Payum\Core\Model\PaymentInterface as ModelPaymentInterface;
-use Payum\Core\Payum;
-use Payum\Core\Security\HttpRequestVerifierInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Tsetsee\Qpay\Api\DTO\CreateInvoiceResponse;
 
-final class QPayQRController extends AbstractController
+final class QPayQRController extends PayumController
 {
-    public function __construct(
-        private OrderRepositoryInterface $orderRepository,
-        private Payum $payum,
-    ) {
-    }
-
-    public function renderQRAction(Request $request): Response
-    {
+    public function renderQRAction(
+        Request $request,
+        OrderRepositoryInterface $orderRepository,
+    ): Response {
         $orderId = $request->attributes->getInt('id');
 
         /** @var OrderInterface $order */
-        $order = $this->orderRepository->find($orderId);
+        $order = $orderRepository->find($orderId);
 
         /** @var ModelPaymentInterface $payment */
         $payment = $order->getLastPayment(PaymentInterface::STATE_PROCESSING);
@@ -42,18 +36,18 @@ final class QPayQRController extends AbstractController
         ]);
     }
 
-    public function callbackAction(Request $request): Response
-    {
-        $token = $this->getHttpRequestVerifier()->verify($request);
-        $gateway = $this->payum->getGateway($token->getGatewayName());
+    public function callbackAction(
+        Request $request,
+        OrderRepositoryInterface $orderRepository,
+    ): Response {
+        /** @var OrderInterface $order */
+        $order = $orderRepository->findOneByTokenValue((string) $request->attributes->get('payum_token'));
+        $gateway = $this->payum->getGateway('QPay Payment');
+
+        dd($order);
 
         // $gateway->execute()
 
         return new Response('SUCCESS');
-    }
-
-    private function getHttpRequestVerifier(): HttpRequestVerifierInterface
-    {
-        return $this->payum->getHttpRequestVerifier();
     }
 }
--- src/Payum/Action/CaptureAction.php
+++ src/Payum/Action/CaptureAction.php
@@ -10,9 +10,12 @@ use Payum\Core\ApiAwareInterface;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Exception\UnsupportedApiException;
 use Payum\Core\Request\Capture;
+use Payum\Core\Request\Generic;
 use Psr\Log\LoggerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\PaymentInterface as SyliusPaymentInterface;
+use Sylius\Component\Payment\Model\PaymentInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Tsetsee\Qpay\Api\DTO\CreateInvoiceRequest;
 use Tsetsee\Qpay\Api\Exception\BadResponseException;
 use Tsetsee\Qpay\Api\QPayApi;
@@ -24,14 +27,17 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
     /** @var SyliusApi */
     private $api;
 
-    public function __construct(private LoggerInterface $logger)
-    {
+    public function __construct(
+        private LoggerInterface $logger,
+        private UrlGeneratorInterface $urlGenerator,
+    ) {
     }
 
     public function execute($request): void
     {
         RequestNotSupportedException::assertSupports($this, $request);
 
+        /** @var Generic $request */
         /** @var SyliusPaymentInterface $payment */
         $payment = $request->getModel();
         /** @var OrderInterface $order */
@@ -49,21 +55,28 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
                 options: ['logger' => $this->logger],
             );
 
-            if ($payment->getAmount() === null) {
-                $details['status'] = QPayPayment::STATE_CANCEL;
-            } else {
-                $invoice = $client->createInvoice(CreateInvoiceRequest::from([
-                    'invoiceCode' => $this->api->getInvoiceCode(),
-                    'senderInvoiceNo' => $order->getNumber(),
-                    'invoiceReceiverCode' => $order->getUser()?->getUsernameCanonical() ?? $order->getNumber(),
-                    'invoiceDescription' => 'purchasement of order ' . $order->getNumber(),
-                    'senderBranchCode' => 'CENTRAL',
-                    'amount' => $payment->getAmount() / 100.0,
-                    'callbackUrl' => '',
-                ]));
+            if ($payment->getState() === PaymentInterface::STATE_NEW) {
+                if ($payment->getAmount() === null) {
+                    $details['status'] = QPayPayment::STATE_CANCEL;
+                } else {
+                    $invoice = $client->createInvoice(CreateInvoiceRequest::from([
+                        'invoiceCode' => $this->api->getInvoiceCode(),
+                        'senderInvoiceNo' => $order->getNumber(),
+                        'invoiceReceiverCode' => $order->getUser()?->getUsernameCanonical() ?? $order->getNumber(),
+                        'invoiceDescription' => 'purchasement of order ' . $order->getNumber(),
+                        'senderBranchCode' => 'CENTRAL',
+                        'amount' => $payment->getAmount() / 100.0,
+                        // 'callbackUrl' => $this->urlGenerator->generate('payum_capture_do', [
+                    //     'payum_token' => $request->getToken()->getHash(),
+                        // ]),
+                        'callbackUrl' => $request->getToken()->getTargetUrl(),
+                    ]));
 
-                $details['status'] = QPayPayment::STATE_PROCESSED;
-                $details['invoice'] = $invoice->toArray();
+                    $details['status'] = QPayPayment::STATE_PROCESSED;
+                    $details['invoice'] = $invoice->toArray();
+                }
+            } elseif ($payment->getState() === PaymentInterface::STATE_PROCESSING) {
+                $invoiceResponse = $client->getInvoice($payment->getDetails()['invoice']['invoiceId']);
             }
         } catch (RequestException $exception) {
             $response = $exception->getResponse();
